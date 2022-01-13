2c6b5d637f6686073e9e7eb26ccaeeca904ecb77
1 <1>
2022-01-13 11:01:14 -0500

    nb

--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -13,7 +13,7 @@ public class SoccerTeam {
     }
     public void played(SoccerTeam other, int myScore, int otherScore){
         numGames++;
-        numGoals = myScore+otherScore;
+        numGoals= myScore+otherScore+numGoals;
         if (myScore>otherScore){
             wins++;
             other.loses++;
@@ -52,9 +52,54 @@ public class SoccerTeam {
         SoccerTeam pats = new SoccerTeam();
         SoccerTeam bucaneers = new SoccerTeam();
 
+        startTournament();
+        System.out.println("tournament 1");
         tigers.played(dolphins,6,7);
-        System.out.println();
+        dolphins.played(pats, 6,6);
+        pats.played(bucaneers, 9,10);
+        bucaneers.played(dolphins, 5,4);
+
+        System.out.println("Tigers Points:"+ tigers.getPoints());
+        System.out.println("Pats Points:"+pats.getPoints());
+        System.out.println("Dolphins Points:"+dolphins.getPoints());
+        System.out.println("Bucaneers Points:"+bucaneers.getPoints());
+        System.out.println("total games:"+getNumGames());
+        System.out.println("total Goals:"+getNumGoals());
+        dolphins.reset();
+        tigers.reset();
+        bucaneers.reset();
+        pats.reset();
+
+        startTournament();
+
+        System.out.println("tournament 2");
+
+        tigers.played(dolphins,8,7);
+        dolphins.played(pats, 6,12);
+        pats.played(bucaneers, 7,10);
+        bucaneers.played(dolphins, 5,19);
+
+        System.out.println("Tigers Points:"+ tigers.getPoints());
+        System.out.println("Pats Points:"+pats.getPoints());
+        System.out.println("Dolphins Points:"+dolphins.getPoints());
+        System.out.println("Bucaneers Points:"+bucaneers.getPoints());
+        System.out.println("total games:"+getNumGames());
+        System.out.println("total Goals:"+getNumGoals());
 
     }
 }
 
+//tournament 1
+//Tigers Points:0
+//Pats Points:1
+//Dolphins Points:4
+//Bucaneers Points:6
+//total games:4
+//total Goals:53
+//tournament 2
+//Tigers Points:3
+//Pats Points:3
+//Dolphins Points:3
+// Bucaneers Points:3
+//total games:4
+//total Goals:74
